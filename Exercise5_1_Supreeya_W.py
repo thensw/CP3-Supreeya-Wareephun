6e00902b54af82c2863744a948803106b441da5f
thensw <supreeya.wp@gmail.com>
2023-10-13 23:14:04 +0700

    Create Exercise5_1_Supreeya_W.py

---
+++ Exercise5_1_Supreeya_W.py
@@ -0,0 +1,8 @@
+# input from user
+num1 = int(input('Enter Number 1 :'))
+num2 = int(input('Enter Number 2 :'))
+# output
+print(f'{num1} + {num2} = {num1 + num2}')
+print(f'{num1} - {num2} = {num1 - num2}')
+print(f'{num1} * {num2} = {num1 * num2}')
+print(f'{num1} / {num2} = {num1 // num2}')
