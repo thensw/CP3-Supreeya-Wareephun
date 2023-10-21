0405586bc9b42b457f1fcb9dc3d5dba32b222d20
thensw <supreeya.wp@gmail.com>
2023-10-22 00:37:57 +0700

    Create lec73_dictApplied.py

---
+++ lec73_dictApplied.py
@@ -0,0 +1,18 @@
+systemMenu = {'rice':30,'chic':15, 'cow':60}
+menuList = []
+while True:
+    menuName = input('please enter menu: ')
+    if menuName.lower() == 'exit':
+        break
+    else:
+        menuList.append([menuName, systemMenu[menuName]])
+
+def showBill():
+    total = 0
+    print("--- Receipt ---")
+    for i in range(len(menuList)):
+        print(menuList[i][0],menuList[i][1])
+        total +=int(menuList[i][1])
+    print(f'Total : {total}')
+
+showBill()
